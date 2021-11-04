4dce181891898cfce9c6b30ce3f2fefd059effe4
Chris Gehrke <31970227+ChrisGehrke@users.noreply.github.com>
2021-11-04 15:38:55 -0600

    Add files via upload
    updated domain layers

--- HousingSelector.java
+++ HousingSelector.java
@@ -6,6 +6,32 @@ public class HousingSelector {
 	private String Conex;
 	private String Printed;
 	
+	public HousingSelector(String prefabricated, String conex, String printed) {
+		super();
+		Prefabricated = prefabricated;
+		Conex = conex;
+		Printed = printed;
+	}
+	
+	public String getPrefabricated() {
+		return Prefabricated;
+	}
+	public void setPrefabricated(String prefabricated) {
+		Prefabricated = prefabricated;
+	}
+	public String getConex() {
+		return Conex;
+	}
+	public void setConex(String conex) {
+		Conex = conex;
+	}
+	public String getPrinted() {
+		return Printed;
+	}
+	public void setPrinted(String printed) {
+		Printed = printed;
+	}
+	
 
 	}
 
---
+++ User.java
@@ -0,0 +1,54 @@
+package com;
+
+public class User {
+	private Integer id;
+	private String email;
+	private String phone;
+	private String address;
+	
+	public User(Integer id, String email, String phone, String address) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+	
+	
+	
+}
