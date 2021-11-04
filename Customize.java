05a1a803a3a90f55c0ce0880bf442e52af3551d6
Chris Gehrke <31970227+ChrisGehrke@users.noreply.github.com>
2021-11-04 15:42:50 -0600

    Add files via upload
    Added cutomizer

---
+++ Customize.java
@@ -0,0 +1,30 @@
+package com;
+
+public class Customize {
+	public String paint;
+	public String upgrade;
+	
+	public Customize(String paint, String upgrade) {
+		super();
+		this.paint = paint;
+		this.upgrade = upgrade;
+	}
+
+	public String getPaint() {
+		return paint;
+	}
+
+	public void setPaint(String paint) {
+		this.paint = paint;
+	}
+
+	public String getUpgrade() {
+		return upgrade;
+	}
+
+	public void setUpgrade(String upgrade) {
+		this.upgrade = upgrade;
+	}
+	
+	
+}
